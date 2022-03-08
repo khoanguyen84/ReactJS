400f2cb2b212f28ddc9168ae02995c9358cefc2f
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-03-08 14:08:39 +0700

    update

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -29,18 +29,178 @@ import { useState } from 'react'
 //   );
 // }
 
+const orders = [200,300,400,500];
 
-function App() {
-  const [counter, setCounter] = useState(1);
-  function onClickHandle(){
-    setCounter(counter + 1);
+// function App() {
+//   // const [counter, setCounter] = useState(orders.reduce((total, value) => total + value));
+//   const [info, setInfo]= useState({
+//     name : "Khoa",
+//     email: "khoa.nguyen@codegym.vn",
+//     gender : true
+//   })
+//   function onClickHandle(){
+//     // setCounter(counter + 1);
+//     // setCounter(preCounter => preCounter + 1)
+//     // setCounter(preCounter => preCounter + 1)
+//     // setCounter(preCounter => preCounter + 1)
+//     // setInfo({
+//     //   ...info,
+//     //   phone: '34534554'
+//     // })
+//     setInfo(preInfo => ({
+//       ...preInfo,
+//       phone: '34534554'
+//     }))
+//   }
+//   return (
+//     <div className="App">
+//       <h1>{JSON.stringify(info)}</h1>
+//       <button onClick={onClickHandle}>Increase</button>
+//     </div>
+//   )
+// }
+// const gifts = [
+//   "CPU i9",
+//   "RAM 32GB RGB",
+//   "RGB Keyboard"
+// ]
+// function App(){
+//   const [gift, setGift] = useState()
+//   const handleGetGift = () => {
+//     const index = Math.floor(Math.random()*gifts.length);
+//     setGift(gifts[index])
+//   }
+//   return (
+//     <div style={{padding: 32}}>
+//       <h1>{gift || "Chưa có phần thưởng"} </h1>
+//       <button onClick={handleGetGift}>Lấy thưởng</button>
+//     </div>
+//   )
+// }
+// function App(){
+//   const [name, setName] = useState('')
+//   const [email, setEmail] = useState('')
+//   const handleSubmit = () => {
+//     console.log({
+//       name,
+//       email
+//     })
+//   }
+//   return (
+//     <div style= {{padding: 20}}>
+//       <input value={name} onChange={e => setName(e.target.value)} />
+//       <br/>
+//       <input value={email} onChange={e => setEmail(e.target.value)} />
+//       <br/>
+//       <button onClick={handleSubmit}>Submit</button>
+//     </div>
+//   )
+// }
+
+// const courses = [
+//   {
+//     id: 1,
+//     name: "HTML, CSS"
+//   },
+//   {
+//     id: 2,
+//     name: "JavaScript"
+//   },
+//   {
+//     id: 3,
+//     name: "ReactJS"
+//   }
+// ]
+// function App(){
+//   const [checked, setChecked] = useState(1)
+//   const handleSubmit = () => {
+//     console.log({
+//       id: checked
+//     })
+//   }
+//   return (
+//     <div style={{padding:20}}>
+//       { courses.map(course => (
+//         <div key={course.id}>
+//           <input type="radio" 
+//             onChange={() => setChecked(course.id)}
+//             checked = {course.id === checked}
+//           />         
+//           {course.name}
+//         </div>
+//       ))}
+//       <button onClick={handleSubmit}>Register</button>
+//     </div>
+//   )
+// }
+
+// function App(){
+//   const [list, setList] = useState([])
+//   const handleChange = (id) => {
+//     setList(prev => {
+//       const ischecked = list.includes(id)
+//       if(ischecked){
+//         return list.filter(item => item !== id);
+//       }
+//       else{
+//         return [...prev, id];
+//       }
+//     })
+//   }
+
+//   const handleSubmit = ()=>{
+//     console.log({
+//       checked: list
+//     })
+//   }
+//   return (
+//     <div style = {{padding: 20}}>
+//       {
+//         courses.map(course => (
+//           <div key={course.id}>
+//             <input 
+//               type="checkbox"
+//               onChange={() => handleChange(course.id)}
+//               checked = {list.includes(course.id)}
+//             />
+//             {course.name}
+//           </div>
+//         ))
+//       }
+//       <button onClick={handleSubmit}>Submit</button>
+//     </div>
+//   )
+// }
+
+function App(){
+  const [jobs, setJobs] = useState(JSON.parse(localStorage.getItem("jobs")) ?? [])
+  const [job, setJob] = useState("")
+  const handleAdd = () => {
+    setJobs(prev => {
+      const newJobs = [...prev, job]
+      const jsonJobs = JSON.stringify(newJobs);
+      localStorage.setItem("jobs", jsonJobs);
+      return newJobs;
+    })
+    setJob("")
+  }
+  const handleRemove = (index) => {
+    jobs.splice(index, 1)
+    localStorage.setItem("jobs", JSON.stringify(jobs));
+    setJobs([...jobs]);
   }
   return (
-    <div className="App">
-      <h1>{counter}</h1>
-      <button onClick={onClickHandle}>Increase</button>
+    <div style={{padding: 32}}>
+        <input value={job} onChange={(e) => setJob(e.target.value)}/>
+        <button onClick={handleAdd}>Add</button>
+        <ul>
+          {
+            jobs.map(
+                (item, index) => <li key={index}>{item} <button onClick={() => handleRemove(index)}>&times;</button></li>
+            )
+          }      
+        </ul>
     </div>
   )
 }
-
 export default App;
